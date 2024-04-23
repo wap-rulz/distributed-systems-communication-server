f7f10f733309e41d59caa213a686b7a0f1081483
adeesha <wap.rulz@gmail.com>
2024-04-23 23:43:45 +0530

    naming service implementation

--- src/main/java/ds/adeesha/communication/server/BankServer.java
+++ src/main/java/ds/adeesha/communication/server/BankServer.java
@@ -1,15 +1,20 @@
 package ds.adeesha.communication.server;
 
+import ds.adeesha.naming.NameServiceClient;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 import java.io.IOException;
 
 public class BankServer {
+    public static final String NAME_SERVICE_ADDRESS = "http://localhost:2379";
+
     public static void main(String[] args) throws IOException, InterruptedException {
-        int serverPort = 11436;
+        int serverPort = 11438;
         Server server = ServerBuilder.forPort(serverPort).addService(new BalanceServiceImpl()).build();
         server.start();
+        NameServiceClient client = new NameServiceClient(NAME_SERVICE_ADDRESS);
+        client.registerService("CheckBalanceService", "127.0.0.1", serverPort, "tcp");
         System.out.println("BankServer Started and ready to accept requests on port: " + serverPort);
         server.awaitTermination();
     }
